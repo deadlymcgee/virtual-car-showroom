756d070937d3ba54a8a81d221397d823adaa1fee
NateSB <nathanpwaters@outlook.com>
2018-01-29 21:03:29 +1100

    -(wip) specs interaction --show specs content --increase section height

---
+++ DIG252_Virtual_Car_Showroom/assets/scripts/specifications.js
@@ -0,0 +1,13 @@
+﻿
+
+
+
+
+$(".div-specs-top-level-menu").click(function () {
+    var sectionHeight = $("#section4").height();
+    var newSectionHeight = String(sectionHeight + 485) + "px !important";
+    $("#section4").removeClass("div-section4-normal")
+    $("#section4").css("height", "+=485");
+    $(".div-specs-content-container").show();
+    $(".div-specs-content-container *").show();
+});
