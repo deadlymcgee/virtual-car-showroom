8f9067dc12541bd7581ef20ba479e10290318292
NateSB <nathanpwaters@outlook.com>
2018-01-29 22:01:55 +1100

    -(wip) specs interaction --scroll interaction into the viewport --reset section

--- DIG252_Virtual_Car_Showroom/assets/scripts/specifications.js
+++ DIG252_Virtual_Car_Showroom/assets/scripts/specifications.js
@@ -4,10 +4,30 @@
 
 
 $(".div-specs-top-level-menu").click(function () {
-    var sectionHeight = $("#section4").height();
-    var newSectionHeight = String(sectionHeight + 485) + "px !important";
-    $("#section4").removeClass("div-section4-normal")
-    $("#section4").css("height", "+=485");
-    $(".div-specs-content-container").show();
-    $(".div-specs-content-container *").show();
+
+    if ($(".div-specs-content-container").is(":hidden")) {
+        console.log("hidden!");
+        $("#section4").removeClass("div-section4-normal")
+        $("#section4").css("height", "+=485");
+        $(".div-specs-content-container").show();
+        $(".div-specs-content-container *").show();
+        //scroll up so the interaction is fully visible
+        $('html, body').animate({
+            scrollTop: $(".div-specs-top-level-menu").offset().top - 70
+        }, 1000);
+        //(Steve, 2011)
+    }
+    else if ($(".div-specs-content-container").is(":visible")) {
+        console.log("reset the interaction!");
+        $("#section4").addClass("div-section4-normal");
+        $("#section4").css("height", "");
+        $(".div-specs-content-container").hide();
+        $(".div-specs-content-container *").hide();
+        $.smoothScroll({
+            scrollTarget: "#specs",
+            speed: 1000
+        });
+    }
+    
+
 });
