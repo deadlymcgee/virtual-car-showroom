87d76b4e82ec2c29bc90cdfc0c6b14a1139f9186
NateSB <nathanpwaters@outlook.com>
2018-01-30 20:36:26 +1100

    -(wip) specs interaction --show submenu

--- DIG252_Virtual_Car_Showroom/assets/scripts/specifications.js
+++ DIG252_Virtual_Car_Showroom/assets/scripts/specifications.js
@@ -3,31 +3,39 @@
 
 
 
-$(".div-specs-top-level-menu").click(function () {
+$(".div-specs-top-level-menu-button-container").click(function () {
 
     if ($(".div-specs-content-container").is(":hidden")) {
         console.log("hidden!");
         $("#section4").removeClass("div-section4-normal")
         $("#section4").css("height", "+=485");
         $(".div-specs-content-container").show();
-        $(".div-specs-content-container *").show();
+        $(".div-specs-second-level-menu-container").show();
+        //$(".div-specs-sub-menu").show();
         //scroll up so the interaction is fully visible
         $('html, body').animate({
             scrollTop: $(".div-specs-top-level-menu").offset().top - 70
         }, 1000);
         //(Steve, 2011)
     }
-    else if ($(".div-specs-content-container").is(":visible")) {
+
+
+    var selected = $(this).attr("id");
+    $(".div-" + selected).addClass("active");
+
+
+});
+
+$(".div-specs-content-close").click(function() {
+    if ($(".div-specs-content-container").is(":visible")) {
         console.log("reset the interaction!");
         $("#section4").addClass("div-section4-normal");
         $("#section4").css("height", "");
         $(".div-specs-content-container").hide();
-        $(".div-specs-content-container *").hide();
+        //$(".div-specs-content-container *").hide();
         $.smoothScroll({
             scrollTarget: "#specs",
             speed: 1000
         });
     }
-    
-
-});
+})
