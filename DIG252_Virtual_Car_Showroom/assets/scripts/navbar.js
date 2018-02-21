19cde116a7de6f493a73ba71e2690931159c38af
NateSB <nathanpwaters@outlook.com>
2018-02-21 15:02:10 +1100

    -navbar --set current section (active) link on page load --set active link depending on when scrolling --do not apply when a smoothscroll scroll is happening

--- DIG252_Virtual_Car_Showroom/assets/scripts/navbar.js
+++ DIG252_Virtual_Car_Showroom/assets/scripts/navbar.js
@@ -1,24 +1,34 @@
 ﻿
+var smoothScrollActive = false;
 
 
-
-
-// change opacity dynamically depending on position
-// increase navbar transparency when on home section
-var mywindow = $(window);
-var section2 = $("#section2");
-$(window).on('scroll',
-    function () {
-        if (mywindow.scrollTop() < section2.offset().top - 50) {
-            // check current state to improve performance
-            $("#navbar").removeClass("navbar-opaque");
-            $("#navbar").addClass("navbar-transparent");
-        } else {
-            $("#navbar").removeClass("navbar-transparent");
-            $("#navbar").addClass("navbar-opaque");
+// control the scrolling
+$('.div-navbar-link').click(function (event) {
+    smoothScrollActive = true;
+    event.preventDefault();
+    $.smoothScroll({
+        scrollTarget: "#" + event.target.innerText.toLowerCase(),
+        speed: 1500,
+        afterScroll: function(options) {
+            smoothScrollActive = false;
         }
     });
-//(taylorleejones, 2013)
+    event.stopPropagation();
+
+    //return false;
+});
+
+
+
+
+
+var mywindow = $(window);
+
+var section2 = $("#section2");
+$(window).on('scroll', function() {
+        navbarProps();
+    });
+
 
 
 
@@ -50,3 +60,69 @@ $(".div-navbar-link").click(function() {
 
     }
 });
+
+// change opacity dynamically depending on position
+// increase navbar transparency when on home section
+// set the active section effect according to scroll position
+// do not apply when a smooth scroll event is in progress
+function navbarProps() {
+    var positionFromTop = mywindow.scrollTop();
+    if (positionFromTop < section2.offset().top - 50) {
+        // check current state to improve performance
+        if (!$("#navbar").hasClass("navbar-transparent")) {
+            $("#navbar").removeClass("navbar-opaque");
+            $("#navbar").addClass("navbar-transparent");
+        }
+    }
+    if (positionFromTop > section2.offset().top - 50) {
+        if (!$("#navbar").hasClass("navbar-opaque")) {
+            $("#navbar").removeClass("navbar-transparent");
+            $("#navbar").addClass("navbar-opaque");
+        }
+    }
+    //(taylorleejones, 2013)
+    // active link effect
+    if (!smoothScrollActive) {
+        if (positionFromTop >= 0 && positionFromTop < section2.offset().top - 50) {
+            var link = $(".div-navbar-link")
+                .filter(function () {
+                    return $(this).children(".navbar-text").text() === "HOME";
+                });
+            if (!$(link).hasClass("active")) {
+                $(".div-navbar-link.active").removeClass("active");
+                $(link).addClass("active");
+            }
+        }
+        if (positionFromTop >= section2.offset().top - 50 && positionFromTop < $("#section3").offset().top - 50) {
+            var link = $(".div-navbar-link")
+                .filter(function () {
+                    return $(this).children(".navbar-text").text() === "FEATURES";
+                });
+            if (!$(link).hasClass("active")) {
+                $(".div-navbar-link.active").removeClass("active");
+                $(link).addClass("active");
+            }
+        }
+        if (positionFromTop >= $("#section3").offset().top - 50 && positionFromTop < $("#section4").offset().top - 50) {
+            var link = $(".div-navbar-link")
+                .filter(function () {
+                    return $(this).children(".navbar-text").text() === "COLOUR";
+                });
+            if (!$(link).hasClass("active")) {
+                $(".div-navbar-link.active").removeClass("active");
+                $(link).addClass("active");
+            }
+        }
+        if (positionFromTop >= $("#section4").offset().top - 50) {
+            var link = $(".div-navbar-link")
+                .filter(function () {
+                    return $(this).children(".navbar-text").text() === "SPECS";
+                });
+            if (!$(link).hasClass("active")) {
+                $(".div-navbar-link.active").removeClass("active");
+                $(link).addClass("active");
+            }
+        }
+    }
+
+}
