bdff7e423b1d9494287832572d6b85daec4d6751
NateSB <nathanpwaters@outlook.com>
2018-02-21 13:08:36 +1100

    -navbar --decrease opacity when on home section

--- DIG252_Virtual_Car_Showroom/assets/scripts/navbar.js
+++ DIG252_Virtual_Car_Showroom/assets/scripts/navbar.js
@@ -2,6 +2,26 @@
 
 
 
+
+// change opacity dynamically depending on position
+// increase navbar transparency when on home section
+var mywindow = $(window);
+var section2 = $("#section2");
+$(window).on('scroll',
+    function () {
+        if (mywindow.scrollTop() < section2.offset().top - 50) {
+            // check current state to improve performance
+            $("#navbar").removeClass("navbar-opaque");
+            $("#navbar").addClass("navbar-transparent");
+        } else {
+            $("#navbar").removeClass("navbar-transparent");
+            $("#navbar").addClass("navbar-opaque");
+        }
+    });
+//(taylorleejones, 2013)
+
+
+
 // hover effect for all links except logo
 $(".div-navbar-link-container > a > .div-navbar-link")
     //.children(".div-navbar-link")
